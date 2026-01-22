c809e34626a851573284095d436055e2903a0691
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2026-01-22 09:14:52 -0300

    feat(server-actions): use server actions with useTransition in client components

--- src/components/admin/DeletePostButton/index.tsx
+++ src/components/admin/DeletePostButton/index.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { deletePostAction } from "@/actions/post/delete-post-action";
 import clsx from "clsx";
 import { Trash2Icon } from "lucide-react";
+import { useTransition } from "react";
 
 type DeletePostButtonProps = {
   id: string;
@@ -9,8 +11,15 @@ type DeletePostButtonProps = {
 };
 
 export function DeletePostButton({ id, title }: DeletePostButtonProps) {
+  const [isPending, startTransition] = useTransition();
+
   function handleClick() {
-    alert("Botão clicado " + id);
+    if (!confirm("Tem certeza?")) return;
+
+    startTransition(async () => {
+      const result = await deletePostAction(id);
+      alert(`O result é: ${result}`);
+    });
   }
 
   return (
@@ -20,10 +29,12 @@ export function DeletePostButton({ id, title }: DeletePostButtonProps) {
         "[&_svg]:w-4 [&_svg]:h-4",
         "hover:text-red-500 hover:scale-110",
         "opacity-70 group-hover:opacity-100",
+        "disabled:text-green-100 disabled:cursor-not-allowed disabled:hover:scale-100",
       )}
       aria-label={`Apagar post: ${title}`}
       title={`Apagar post: ${title}`}
       onClick={handleClick}
+      disabled={isPending}
     >
       <Trash2Icon />
     </button>
